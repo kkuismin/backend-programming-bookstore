e03c8d514739ef7f050491dd2eebd47722803998
kkuismin <katariina.kuismin@gmail.com>
2023-02-01 20:24:08 +0200

    Adding delete functionality

--- src/main/java/backend/programming/Bookstore/web/BookController.java
+++ src/main/java/backend/programming/Bookstore/web/BookController.java
@@ -4,8 +4,10 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 
 import backend.programming.Bookstore.domain.Book;
 import backend.programming.Bookstore.domain.BookRepository;
@@ -38,5 +40,11 @@ public class BookController {
 	public String saveBook(Book book, Model model) {
 		bookRepository.save(book);
 		return "redirect:/booklist";
+	}
+	
+	@RequestMapping(value = "/delete/{id}", method = RequestMethod.GET)
+	public String deleteStudent(@PathVariable("id") Long bookId, Model model) {
+	 bookRepository.deleteById(bookId);
+	 return "redirect:../booklist";
 	}
 }
